4a93c3c5efa51421197cb822b8233c41ba3da41e
Aditi Tiwari <adititiwari1204@gmail.com>
2025-10-24 21:25:02 +0530

    Delete healthcheck.sh

--- healthcheck.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Simple healthcheck script: return 0 if root path returns HTTP 2xx/3xx
-URL="http://127.0.0.1:80/"
-
-# -s silent, -S show errors, -f fail on non-2xx/3xx
-curl -sS -f "$URL" >/dev/null 2>&1
-exit $?
